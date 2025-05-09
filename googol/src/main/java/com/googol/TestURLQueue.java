e388a4a68340bf626f7efa78f2299e2cb4e431f0
BernardoSilva <bernardocavsilva@gmail.com>
2025-05-09 21:22:04 +0100

    Update TestURLQueue.java

--- googol/src/main/java/com/googol/TestURLQueue.java
+++ googol/src/main/java/com/googol/TestURLQueue.java
@@ -11,7 +11,7 @@ public class TestURLQueue {
             Registry registry = LocateRegistry.getRegistry("localhost", 1088);
             URLQueueInterface queue = (URLQueueInterface) registry.lookup("URLQueue");
 
-            queue.addURL("https://pt.wikipedia.org/wiki/Ferrari"); // URL de teste
+            queue.addURL("https://en.wikipedia.org/wiki/Ferrari"); // URL de teste
 
             System.out.println("URL added to queue!");
             System.out.println("Size right now: " + queue.getQueueSize());
