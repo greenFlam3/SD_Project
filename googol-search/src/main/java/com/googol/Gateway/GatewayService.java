3f501a7d00597ff916ad1b8fbe0b2f84a54dfb23
BernardoSilva <bernardocavsilva@gmail.com>
2025-05-15 22:04:16 +0100

    quick name change

---
+++ googol-search/src/main/java/com/googol/Gateway/GatewayService.java
@@ -0,0 +1,14 @@
+package com.googol.Gateway;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import com.googol.Storage.PageInfo;
+
+public interface GatewayService extends Remote {
+    void indexPage(String url, String title, String text) throws RemoteException;
+    List<String> smartSearch(String userQuery) throws RemoteException;
+    PageInfo getPageSummary(String url) throws RemoteException;
+    List<String> getTopSearches() throws RemoteException;
+    List<BarrelStat> getBarrelStats() throws RemoteException;
+}
