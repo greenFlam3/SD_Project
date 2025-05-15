28420cfb0104464db462b9dfd341bacba02f50ad
BernardoSilva <bernardocavsilva@gmail.com>
2025-05-15 21:59:25 +0100

    BarrelStats

--- googol/src/main/java/com/googol/Gateway/GatewayService.java
+++ googol/src/main/java/com/googol/Gateway/GatewayService.java
@@ -10,4 +10,5 @@ public interface GatewayService extends Remote {
     List<String> smartSearch(String userQuery) throws RemoteException;
     PageInfo getPageSummary(String url) throws RemoteException;
     List<String> getTopSearches() throws RemoteException;
+    List<BarrelStat> getBarrelStats() throws RemoteException;
 }
